8d1adf63d34ebd4a42b69dae6d58d14f18d05eca
skidev101 <skidev101@gmail.com>
2025-07-03 23:56:12 +0100

    updated username validator util

--- frontend/src/utils/validateUsername.ts
+++ frontend/src/utils/validateUsername.ts
@@ -1,9 +1,31 @@
-export const validateUsername = (username: string): string | null => {
-  if (username.includes(" ")) return "Username cannot contain spaces";
+import emojiRegex from "emoji-regex";
 
-  if (username.trim().length < 3) return "Username must be at least 3 characters";
+const emojiPattern = emojiRegex();
 
-  if (username.trim().length > 10) return "Username must be less than 10 characters";
+
+export const validateUsername = (value: string): string | null => {
+  const username = value.trim();
+
+  const emojiMatches = username.match(emojiPattern) || [];
+  const textWithoutEmojis = username.replace(emojiPattern, '');
+  const visualLength = emojiMatches.length + textWithoutEmojis.length;
+
+  if (visualLength < 3) return "Username must be at least 3 characters";
+
+  if (visualLength > 10) return "Username must be less than 10 characters";
+
+
+  const cleanUsername = username.replace(emojiPattern, '');
+  const emojiMatch = /^[\p{L}\p{N}_-]*$/u.test(cleanUsername);
+
+  // const valid = /^[\p{L}\p{N}_-]+/u;
+
+  // const emojiMatch = [...username].every(
+  //   (char) => valid.test(char) || emojiPattern.test(char)
+  // )
+
+  if (!emojiMatch) return "Username cannot contain spaces";
+
 
   return null;
 };
