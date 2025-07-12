8dfc9658fcb4acefffbb790ba17800f9247886db
skidev101 <skidev101@gmail.com>
2025-07-12 01:05:49 +0100

    created groupByDate util

---
+++ frontend/src/utils/groupByDate.ts
@@ -0,0 +1,37 @@
+interface RecentLink {
+   _id: string;
+   url: string,
+   createdAt: string,
+   topic: string
+}
+
+export const groupLinksByDate = (links: RecentLink[]): Record<string, RecentLink[]> => {
+   const grouped: Record<string, RecentLink[]> = {};
+   const todayStr = new Date().toDateString();
+   const yesterdayStr = new Date(Date.now() - 24 * 60 * 60 * 1000).toDateString();
+
+   links.forEach(link => {
+      const linkDateStr = new Date(link.createdAt).toDateString();
+      let dateLabel: string;
+
+      if (linkDateStr === todayStr) {
+         dateLabel ='Today';
+      } else if (linkDateStr === yesterdayStr) {
+         dateLabel = 'Yesterday';
+      } else {
+         dateLabel = new Date(link.createdAt).toLocaleString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+         });
+      }
+
+      if (!grouped[dateLabel]) {
+         grouped[dateLabel] = [];
+      }
+
+      grouped[dateLabel].push(link)
+   });
+
+   return grouped
+}
