c0c848fd1050d4bfa4a46a86fbdcca0905329e4f
skidev101 <skidev101@gmail.com>
2025-08-21 01:37:05 +0100

    modified socialshare btns

--- frontend/src/features/messages/components/SocialShareButtons/SocialShareButtons.tsx
+++ frontend/src/features/messages/components/SocialShareButtons/SocialShareButtons.tsx
@@ -1,4 +1,3 @@
-import { MessageCircle, Send, Share2 } from "lucide-react";
 
 interface ShareProps {
   url: string;
@@ -35,18 +34,18 @@ const SocialShareButtons = ({ url, text }: ShareProps) => {
       <div className="flex items-center max-w-max bg-gray-100 rounded-3xl gap-3 p-3 shadow-sm">
         <button
           onClick={() => handleShare("whatsapp")}
-          className="bg-gray-200 rounded-full p-2 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
-          <MessageCircle size={25} />
+          className="bg-gray-200 rounded-full p-1 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
+          <img src="/whatsapp.png" alt="whatsapp" className="w-8 h-8 sm:w-12 sm:h-12" />
         </button>
         <button
           onClick={() => handleShare("twitter")}
-          className="bg-gray-200 rounded-full p-2 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
-          <Send size={25} />
+          className="bg-gray-200 rounded-full p-1 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
+          <img src="/twitter.png" alt="twitter" className="w-8 h-8 sm:w-12 sm:h-12" />
         </button>
         <button
           onClick={() => handleShare("facebook")}
-          className="bg-gray-200 rounded-full p-2 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
-          <Share2 size={25} />
+          className="bg-gray-200 rounded-full p-1 hover:scale-105 active:scale-95 cursor-pointer transition-all hover:bg-gray-300">
+          <img src="/facebook.png" alt="facebook" className="w-8 h-8 sm:w-12 sm:h-12" />
         </button>
       </div>
     </div>
