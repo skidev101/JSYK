07a30eb58e27e4b6d18ad1149e2ec48842fd259d
skidev101 <skidev101@gmail.com>
2025-08-29 21:12:42 +0100

    modified vercel config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ const DBConnect = async () => {
    try {
       if (isConnected) return;
       await mongoose.connect(process.env.MONGO_URI);
-      isConnected = true;
+      isConnected = conn.connections[0].readyState === 1;
       console.log('MongoDB connected successfully');
    } catch (err) {
       console.error('MongoDB connection error:', err);
