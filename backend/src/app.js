487ba717c5fd9d7e81ace21b5c5658b275be4565
skidev101 <skidev101@gmail.com>
2025-09-07 16:02:24 +0100

    modified index.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,28 +11,12 @@ import topicRoutes from "./routes/topicRoutes.js";
 import imageRoute from "./routes/imageRoute.js";
 import userRoutes from "./routes/userRoutes.js";
 import ogRoutes from "./routes/ogRoutes.js";
-// import { imageCleanupJob } from "./jobs/imageCleaner.js";
+import { imageCleanupJob } from "./jobs/imageCleaner.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("jsyk backend is now live!");
-});
-
-app.get("/api/cleanup-images", async (req, res) => {
-  try {
-    const { secret } = req.query;
-
-    if (secret !== process.env.CRON_SECRET) {
-      return res.status(403).json({ success: false, message: "Forbidden" });
-    }
-    
-    await imageCleanupJob();
-    console.log("cleanup done");
-    res.json({ success: true, message: "Cleanup executed" });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
 });
 
 app.use(cors(corsConfig));
@@ -50,5 +34,21 @@ app.use("/api/image/sign", imageRoute);
 app.use("/api/user", userRoutes);
 app.use("/api/og-image", ogRoutes);
 app.use("/api/share", ogRoutes);
+
+app.get("/api/cleanup-images", async (req, res) => {
+  try {
+    const { secret } = req.query;
+
+    if (secret !== process.env.CRON_SECRET) {
+      return res.status(403).json({ success: false, message: "Forbidden" });
+    }
+
+    await imageCleanupJob();
+    console.log("cleanup done");
+    res.json({ success: true, message: "Cleanup executed" });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 export default app;
