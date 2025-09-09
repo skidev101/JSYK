1ebb1db9b179e041fee609bef8fd19fa6036f02f
skidev101 <skidev101@gmail.com>
2025-09-09 23:44:40 +0100

    merged analytics

--- frontend/src/features/Admin/components/Analytics/Analytics.tsx
+++ frontend/src/features/Admin/components/Analytics/Analytics.tsx
@@ -12,18 +12,17 @@ const Analytics = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 gap-2">
         <FadeIn delay={0.1}>
-
-        <Card className="hover:bg-gray-200 transition-all">
-          <div className="flex items-center gap-3">
-            <div className="bg-gray-200 text-gray-700 p-4 rounded-2xl">
-              <UserPlus size={21} />
+          <Card className="hover:bg-gray-200 transition-all">
+            <div className="flex items-center gap-3">
+              <div className="bg-gray-200 text-gray-700 p-4 rounded-2xl">
+                <UserPlus size={21} />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-base text-gray-700">New Users today</p>
+                <h1 className="text-xl font-black text-blue-600">82</h1>
+              </div>
             </div>
-            <div className="flex flex-col">
-              <p className="text-base text-gray-700">New Users today</p>
-              <h1 className="text-xl font-black text-blue-600">82</h1>
-            </div>
-          </div>
-        </Card>
+          </Card>
         </FadeIn>
         <Card>
           <div className="flex items-center gap-3">
